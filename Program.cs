443548517b95cd19e3ce2528f9751593ab1e78ec
RadiantGH <iangrapeo@gmail.com>
2022-01-08 21:50:02 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blackjack_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string written = Console.ReadLine();
+        }
+    }
+}
